09642d50f629759378b13303399ee264d3bda884
xkcm <xkcm16@gmail.com>
2022-06-07 10:28:56 +0200

    Add linting

--- app.js
+++ app.js
@@ -1,39 +1,39 @@
-const request = require('request');
+const request = require("request");
 
-function sendRequest(action){
-    let auth;
-    if (action.params.bearerToken){
-        auth = {
-            bearer: action.params.bearerToken,
-            sendImmediately : true
-        }
-    } else if (action.params.username || action.params.password){
-        auth = {
-            user: action.params.username,
-            pass: action.params.password,
-            sendImmediately : true
-        }
-    }
-
-    const requestOptions = {
-        url : action.params.url,
-        method : action.params.method || "GET",
-        body : action.params.body || undefined,
-        json : true,
-        headers: action.params.headers || {},
-        auth : auth
+function sendRequest(action) {
+  let auth;
+  if (action.params.bearerToken) {
+    auth = {
+      bearer: action.params.bearerToken,
+      sendImmediately: true,
     };
-    
-    return new Promise((resolve,reject)=>{
-        request(requestOptions,function (err, response, body) {
-            if(err){
-                return reject(err);
-            }
-            resolve(response);
-        });
-    })
+  } else if (action.params.username || action.params.password) {
+    auth = {
+      user: action.params.username,
+      pass: action.params.password,
+      sendImmediately: true,
+    };
+  }
+
+  const requestOptions = {
+    url: action.params.url,
+    method: action.params.method || "GET",
+    body: action.params.body || undefined,
+    json: true,
+    headers: action.params.headers || {},
+    auth,
+  };
+
+  return new Promise((resolve, reject) => {
+    request(requestOptions, (err, response) => {
+      if (err) {
+        return reject(err);
+      }
+      return resolve(response);
+    });
+  });
 }
 
 module.exports = {
-    sendRequest: sendRequest
-}
+  sendRequest,
+};
