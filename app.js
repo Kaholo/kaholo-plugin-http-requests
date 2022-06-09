37c17582e914c185ff2155dbe24277539d6f7ed2
maciek <42122011+xkcm@users.noreply.github.com>
2022-06-09 16:13:56 +0200

    KP-782 Flatten and add linter (#5)

---
+++ app.js
@@ -0,0 +1,48 @@
+const kaholoPluginLibrary = require("@kaholo/plugin-library");
+const request = require("request");
+
+function sendRequest({
+  bearerToken,
+  username,
+  password,
+  url,
+  method,
+  body,
+  headers,
+}) {
+  let auth;
+  if (bearerToken) {
+    auth = {
+      bearer: bearerToken,
+      sendImmediately: true,
+    };
+  } else if (username || password) {
+    auth = {
+      user: username,
+      pass: password,
+      sendImmediately: true,
+    };
+  }
+
+  const requestOptions = {
+    url,
+    method,
+    body,
+    auth,
+    headers: headers || {},
+    json: true,
+  };
+
+  return new Promise((resolve, reject) => {
+    request(requestOptions, (err, response) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(response);
+    });
+  });
+}
+
+module.exports = kaholoPluginLibrary.bootstrap({
+  sendRequest,
+});
